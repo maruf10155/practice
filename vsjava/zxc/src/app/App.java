2da3e6f3e23e7a00d2aa0ae26c250629b8f0a744
maruf <zxc>
2020-04-23 19:33:43 -0400

    add selenium to vs code

---
+++ vsjava/zxc/src/app/App.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\maruf\\Desktop\\prac\\webdriver\\chromedriver.exe");
+        WebDriver d = new ChromeDriver();
+        d.manage().window().maximize();
+        String s = "http://facebook.com/";
+        d.get(s);
+        System.out.println(d.getTitle());
+        d.findElement(By.id("u_0_b")).click();
+        System.out.println(d.getTitle());
+        // d.close();
+    }
+}
