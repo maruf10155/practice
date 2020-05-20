d30ba2908ef13186c19521a28c9a19d40b52d64e
maruf <you@example.com>
2020-05-20 18:16:30 -0400

    add vs code folder

---
+++ selenium eclipse/asdf/src/test/java/samplemaven/asdf/test.java
@@ -0,0 +1,10 @@
+package samplemaven.asdf;
+
+import org.testng.annotations.Test;
+
+public class test {
+	@Test
+	public void t() {
+	System.out.println(45);
+	}
+}
--- selenium idea/test/src/test1/a.java
+++ selenium idea/test/src/test1/a.java
@@ -8,6 +8,6 @@ public class a {
     @Test
     public void q(){
         System.out.println("4555");
-        Assert.assertEquals(1,1);
+        Assert.assertEquals(1,1)git ;
     }
 }
